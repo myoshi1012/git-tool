b3ac9491337f65cd611d3129ae10f603753e4f2f
Masaya Yoshikawa <aokage000@gmail.com>
2019-12-28 12:10:11 +0900

    enable eslint & reset --hard before pull

--- git/pull.js
+++ git/pull.js
@@ -1,26 +1,35 @@
-const git = require("simple-git/promise");
-const path = require("path");
+const git = require('simple-git/promise')
+const path = require('path')
+const config = require('../config')
 
-const config = require("../config");
-const localPath = process.env.REPO_PATH;
+const localPath = process.env.REPO_PATH
 
 const pull = localRepo =>
   new Promise((resolve, reject) => {
-    const cli = git(localRepo);
+    const cli = git(localRepo)
     cli
       .pull()
       .then(() => resolve(localRepo))
-      .catch(err => reject(err, localRepo));
-  });
+      .catch(err => reject(err, localRepo))
+  })
+
+const resetHard = localRepo =>
+  new Promise((resolve, reject) => {
+    const cli = git(localRepo)
+    cli
+      .reset('hard')
+      .then(() => resolve(localRepo))
+      .catch(err => reject(err, localRepo))
+  })
 
 const main = () => {
   if (!localPath) {
-    console.log("REPO_PATHを設定してね");
+    console.log('REPO_PATHを設定してね')
   }
 
-  const localRepos = config.repos.map(repo => path.join(localPath, repo));
-  const promises = localRepos.map(repo => pull(repo));
-  Promise.allSettled(promises).then(data => console.log(data));
-};
+  const localRepos = config.repos.map(repo => path.join(localPath, repo))
+  const promises = localRepos.map(repo => resetHard(repo).then(localRepo => pull(localRepo)))
+  Promise.allSettled(promises).then(data => console.log(data))
+}
 
-main();
+main()
